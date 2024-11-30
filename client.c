688caf6ac9b9b32f05d50b3129e95d5cf82df8c2
arajshah <araj.shah@bison.howard.edu>
2024-11-30 10:57:04 -0500

    Revision

--- client.c
+++ client.c
@@ -1,120 +1,76 @@
-#include <unistd.h> 
+#include <netinet/in.h> //structure for storing address information 
 #include <stdio.h> 
-#include <sys/socket.h>
-#include <sys/types.h> 
 #include <stdlib.h> 
-#include <netinet/in.h> 
 #include <string.h>
-#include <netdb.h>
+#include <sys/socket.h> //for socket APIs 
+#include <sys/types.h> 
+  
+#define PORT 9001
+#define MAX_COMMAND_LINE_LEN 1024
 
-#define PORT 8000
-#define SIZE 10
+char* getCommandLine(char *command_line){
 
-void receiver(int);
-void sender(int);
+	do{ 
 
-int sfd;
-char buf[256];
-
-int main()
-{
-	struct sockaddr_in s;
-	char ip[20];
-	int port;
-	sfd=socket(PF_INET,SOCK_STREAM,0);//0 signifies TCP
-
-	s.sin_family=AF_INET;
-	// s.sin_port=htons(PORT);
-	// s.sin_addr.s_addr=INADDR_ANY;
-	printf("Enter IP : ");
-	scanf("%s",&ip);
-	printf("Enter Port : ");
-	scanf("%d",&port);
-	
-	s.sin_port=htons(port);
-	s.sin_addr.s_addr = inet_addr(ip);
-
-	int cs=connect(sfd,(struct sockaddr*)&s,sizeof(s));
-	
-	if(cs!=-1)
-		printf("Connection established\n");
-
-	//Create the thread
-	pthread_t tid1,tid2;
-	//pthread_t pid[SIZE+1];
-	// pthread_attr_t attr;
-
-	// pthread_attr_init(&attr);
-
-	memset(buf,NULL,sizeof(buf));
-
-	//Call the threads
-	pthread_create(&tid1,NULL,&sender,0);
-	pthread_create(&tid2,NULL,&receiver,0);
-
-	//Join the threads
-	pthread_join(tid1,NULL);
-	pthread_join(tid2,NULL);
-
-	//Close the file descriptor
-	close(sfd);
+            // Read input from stdin and store it in command_line. If there's an
+            // error, exit immediately. (If you want to learn more about this line,
+            // you can Google "man fgets")
+        
+            if ((fgets(command_line, MAX_COMMAND_LINE_LEN, stdin) == NULL) && ferror(stdin)) {
+                fprintf(stderr, "fgets error");
+                exit(0);
+            }
+ 
+        }while(command_line[0] == 0x0A);  // while just ENTER pressed
+        command_line[strlen(command_line) - 1] = '\0';
+				return command_line;
 }
 
-void receiver(int d)
-{
-	int num;
-	while(1)
-	{
-		if(recv(sfd,&buf,sizeof(buf),0)==0)
-		{
-			close(sfd);
-			exit(0);
-		}
+int main(int argc, char const* argv[]) 
+{ 
+    int sockID = socket(AF_INET, SOCK_STREAM, 0); 
+    char  *token, *cp;
+    char buf[MAX_COMMAND_LINE_LEN];
+		char responeData[MAX_COMMAND_LINE_LEN];
 
-		num=buf[0]-'0';
+    struct sockaddr_in servAddr; 
+  
+    servAddr.sin_family = AF_INET; 
+    servAddr.sin_port 
+        = htons(PORT); // use some unused port number 
+    servAddr.sin_addr.s_addr = INADDR_ANY; 
+  
+    int connectStatus 
+        = connect(sockID, (struct sockaddr*)&servAddr, 
+                  sizeof(servAddr)); 
+  
+    if (connectStatus == -1) { 
+        printf("Error...\n"); 
+    } 
+    else { 
+  
+			while(1) {
+			  printf("Enter Command (or menu): ");
+        getCommandLine(buf);
 
-		if(!((num>=46 && num<=71) || (num>=14 && num<=39)))
-		{
-			memcpy(buf,buf+1,sizeof(buf));
-			buf[strlen(buf)]='\0';
-			printf("Message received from client no. %d : %s\n",num-sfd,buf);
-		}
+				send(sockID, buf, strlen(buf), 0);  // send command and args to server
 
-		else
-			printf("Message received from server : %s\n",buf);
+				cp = buf;
+        token = strtok(cp, " ");
 
-		memset(buf,NULL,sizeof(buf));
-	}
-}
-
-void sender(int d)
-{
-	char cl,dum;
-	while(1)
-	{
-		memset(buf,NULL,sizeof(buf));
-		//scanf("%s",buf);
-		scanf(" %[^\t\n]s",&buf);
-		if(strcmp(buf,"q")==0)
-		{
-			close(sfd);
-			exit(0);
-		}
-		//memcpy(buf+1,buf,sizeof(buf));
-
-		printf("Press 0 to send to server, or the client number : ");
-		scanf("%c %c",&dum,&cl);
-
-		for(int i=strlen(buf)-1;i>=0;i--)
-			buf[i+1]=buf[i];
-
-		buf[strlen(buf)+1]='\0';
-		buf[0]=cl;
-		// printf("Buf[0] : %c\n",buf[0]);
-		
-		// printf("Buffer : %s",buf);
-
-		if(strlen(buf)!=0)
-			send(sfd,&buf,sizeof(buf),0);
-	}
+				if(strcmp(token,"exit") == 0){
+					exit(1);
+				}
+				else if(strcmp(token,"menu") == 0){
+					printf("COMMANDS:\n---------\n1. print\n2. get_length\n3. add_back <value>\n4. add_front <value>\n5. add_position <index> <value>\n6. remove_back\n7. remove_front\n8. remove_position <index>\n9. get <index>\n10. exit\n");
+				}
+ 
+        recv(sockID, responeData, sizeof(responeData), 0); // receive response from server
+  
+        printf("\nSERVER RESPONSE: %s\n", responeData); 
+				memset(buf, '\0', MAX_COMMAND_LINE_LEN);
+			}
+    } 
+ 
+    return 0; 
 }
